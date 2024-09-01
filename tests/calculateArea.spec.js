468b8a52ea0407d17b5ffad1b777ecc03933f28a
enriquepaez <enrique.paezpozo@gmail.com>
2024-09-01 23:04:49 +0200

    Lab finished

--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,31 @@
-// UNCOMMENT THE CODE BELOW TO START
-
-/*
-
 describe("Iteration 3", () => {
     describe("Function - calculateArea", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined", () => {
+            expect(calculateArea).toBeDefined();
+        });
 
-    })    
-})
+        it("should take two numbers as arguments", () => {
+            expect(calculateArea.length).toBe(2);
+        });
 
-*/
+        it("should return a number representing the area of a rectangle (the product of the two arguments)", () => {
+            expect(calculateArea(3, 2)).toEqual(6);
+            expect(calculateArea(5, 5)).toEqual(25);
+            expect(calculateArea(10, 3)).toEqual(30);
+        });
+
+        it("should return undefined if any of the arguments is not provided", () => {
+            expect(calculateArea(1)).toEqual(undefined);
+            expect(calculateArea()).toEqual(undefined);
+            expect(calculateArea(undefined, 1)).toEqual(undefined);
+        });
+
+        it("should return undefined if any of the arguments is not a number", () => {
+            expect(calculateArea(3, "2")).toEqual(undefined);
+            expect(calculateArea("5", 5)).toEqual(undefined);
+            expect(calculateArea("100", "3")).toEqual(undefined);
+        });
+    });    
+});
